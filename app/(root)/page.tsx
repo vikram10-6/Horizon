3244b9fb775411ad433004bc6b53e699841c9c34
vikram10-6 <Vikramayer19@gmail.com>
2024-08-07 15:42:32 +0530

    Ad cards to right side

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import HeaderBox from "@/components/HeaderBox";
 import React from "react";
+import RightSidebar from "@/components/RightSidebar";
 
 const Home = () => {
-  const loggedIn = { firstName: "Vikram" };
+  const loggedIn = { firstName: "Vikram", lastName: "Ayer", email:'Vikramayer19@gmail.com'};
 
   return (
     <section className="home">
@@ -22,7 +23,9 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTION
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance:123.50},{currentBalance:2345.20}]} />
     </section>
   );
 };
