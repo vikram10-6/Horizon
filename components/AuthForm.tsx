3334113e59cf75f9ada9ba3ea59d9b8f98cffade
vikram10-6 <Vikramayer19@gmail.com>
2024-08-08 02:02:47 +0530

    Implement form(basic)

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -3,9 +3,38 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
+
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
+
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof authFormSchema>) {
+    console.log(values);
+  }
 
   return (
     <section className="auth-form">
@@ -36,7 +65,18 @@ const AuthForm = ({ type }: { type: string }) => {
       {user ? (
         <div className="flex flex-col gap-4">{/* BAnK*/}</div>
       ) : (
-        <>FORM</>
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+                <CustomInput control={form.control} name='email' label='Email' placeholder="Enter your Email"/>
+
+                <CustomInput control={form.control} name="password" label='Password' placeholder="Enter your Password"/>
+
+              <Button type="submit" className="form-btn">Submit</Button>
+            </form>
+          </Form>
+        </>
       )}
     </section>
   );
